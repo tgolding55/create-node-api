f7c1c294bb2e89d52ff1c74c8ca40987fc990b52
tgolding55 <tgolding55@gmail.com>
2022-07-26 11:29:18 +0100

    Added createNodeApi for generation, added a .env.example file.

---
+++ README.md
@@ -0,0 +1,9 @@
+Create a .env file before prod
+Change all .env variables before use
+
+To start in prod use npm start
+To start in dev use npm run start:dev
+To run tests use npm test
+
+To seed in dev use npm seed
+To seed in prod manually run "NODE_ENV='prod' node seedDB.js", seeds/seedProd.js will be used.
---
+++ bin/createNodeApi.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+const execSync = require("child_process");
+
+const runCommand = command => {
+  try {
+    execSync(command, { stdio: "inherit" });
+    return true;
+  } catch (err) {
+    console.error(err);
+    return false;
+  }
+};
+
+const repoName = process.argv[2];
+const repo = "https://github.com/tgolding55/create-node-api.git";
+const cloneCommand = `git clone ${repo} ${repoName}`;
+const installCommand = `cd ${repoName} && npm install`;
+
+console.log(`Cloning node api into ${repoName}`);
+
+const cloned = runCommand(cloneCommand);
+if (!cloned) process.exit(-1);
+
+console.log("Installing packages");
+
+const installed = runCommand(installCommand);
+if (!installed) process.exit(-1);
+
+console.log("Node api created.");
