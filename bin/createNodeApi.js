b7d96fbe3874c4d50d3e77be68ae55a07e7bccdf
tgolding55 <tgolding55@gmail.com>
2022-07-27 05:55:17 +0100

    Moved package.json cleanup

--- bin/createNodeApi.js
+++ bin/createNodeApi.js
@@ -28,15 +28,6 @@ const runCommands = async () => {
   const cloned = await runCommand(cloneCommand);
   if (!cloned) process.exit(-1);
 
-  console.log("Installing packages");
-
-  const installed = await runCommand(installCommand);
-  if (!installed) process.exit(-1);
-
-  console.log("Cleaning up");
-  const cleanedUp = await runCommand(removeDirCommand);
-  if (!cleanedUp) process.exit(-1);
-
   const fileName = "package.json";
   const file = JSON.parse(fs.readFileSync(fileName).toString());
 
@@ -46,6 +37,15 @@ const runCommands = async () => {
   file.description = "";
 
   fs.writeFileSync(fileName, JSON.stringify(file));
+
+  console.log("Installing packages");
+
+  const installed = await runCommand(installCommand);
+  if (!installed) process.exit(-1);
+
+  console.log("Cleaning up");
+  const cleanedUp = await runCommand(removeDirCommand);
+  if (!cleanedUp) process.exit(-1);
 
   console.log("Node api created.");
 };
