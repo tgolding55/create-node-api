9fab48478eae84bafc19975a734916442a09f50e
tgolding55 <tgolding55@gmail.com>
2022-08-07 02:09:50 +0100

    Added line to copy .env from .env.example on generate

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ This is a simple node api boilerplate, includes a user model with authorization 
 npx @tgolding55/create-node-api my-app
 ```
 
-Next, fill out the dev.env and test.env files with your environment variables.
+Next, fill out the .env, dev.env and test.env files with your environment variables.
 
 # To test run:
 
@@ -24,7 +24,7 @@ npm test
 npm run start:dev
 ```
 
-# To start in prod, you must first make a .env file based on the .env.example. Then you can use:
+# To start in prod, run:
 
 ```
 npm start
--- bin/createNodeApi.js
+++ bin/createNodeApi.js
@@ -19,24 +19,32 @@ const repoName = process.argv[2];
 const repo = "https://github.com/tgolding55/create-node-api.git";
 const cloneCommand = `git clone ${repo} ${repoName}`;
 const installCommand = `cd ${repoName} && npm install`;
+const createEnv = `cp ${repoName}/.env.example ${repoName}/.env`;
 const removeDirCommand = `rm ${repoName}/bin/createNodeApi.js && rmdir ${repoName}/bin`;
 
 const runCommands = async () => {
-  console.log(`Cloning node api into ${repoName}`);
+  try {
+    console.log(`Cloning node api into ${repoName}`);
 
-  const cloned = await runCommand(cloneCommand);
-  if (!cloned) process.exit(-1);
+    const cloned = await runCommand(cloneCommand);
+    if (!cloned) process.exit(-1);
 
-  console.log("Installing packages");
+    console.log("Installing packages");
+    const installed = await runCommand(installCommand);
+    if (!installed) process.exit(-1);
 
-  const installed = await runCommand(installCommand);
-  if (!installed) process.exit(-1);
+    console.log("Creating .env file");
+    const created = await runCommand(createEnv);
+    if (!created) process.exit(-1);
 
-  console.log("Cleaning up");
-  const cleanedUp = await runCommand(removeDirCommand);
-  if (!cleanedUp) process.exit(-1);
+    console.log("Cleaning up");
+    const cleanedUp = await runCommand(removeDirCommand);
+    if (!cleanedUp) process.exit(-1);
 
-  console.log("Node api created.");
+    console.log("Node api created.");
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 runCommands();
